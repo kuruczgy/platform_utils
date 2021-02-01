9152cec0a2a333904e7dda766ae9e19adc54386e
György Kurucz <kuruczgyurci@hotmail.com>
2021-02-01 18:03:40 +0100

    Add pu_log_trace

--- include/platform_utils/log.h
+++ include/platform_utils/log.h
@@ -4,4 +4,7 @@
 void pu_log_out(const char *fmt, ...);
 void pu_log_info(const char *fmt, ...);
 
+#define pu_log_trace(fmt, ...) \
+	pu_log_info("[trace] %s:%d: " fmt, __func__, __LINE__, __VA_ARGS__)
+
 #endif
